ef42e176a545722e1f17bdd98885ee8266ecc8ae
Isaac Bruno <112228099+isaacbrunodev@users.noreply.github.com>
2023-07-02 22:29:40 -0300

    Update tabela.js
    Registrada a função registrarChamado()

--- tabela.js
+++ tabela.js
@@ -1,5 +1,37 @@
 
-var btn = document.getElementById("meuBotao");
-btn.addEventListener("click", function() {
-  alert("Botão clicado!");
-});
+// Função para registrar um novo chamado
+function registrarChamado() {
+  var idChamado = document.getElementById("id-chamado").value;
+  var unidade = document.getElementById("unidade").value;
+  var descricao = document.getElementById("descricao").value;
+  var estado = document.getElementById("estado").value;
+
+  // Adicionar uma nova linha à tabela com os valores dos campos
+  var tabela = document.getElementById("tabela-chamados");
+  var linha = tabela.insertRow();
+  var colunaIdChamado = linha.insertCell(0);
+  var colunaUnidade = linha.insertCell(1);
+  var colunaDescricao = linha.insertCell(2);
+  var colunaEstado = linha.insertCell(3);
+  var colunaEditar = linha.insertCell(4);
+  var colunaExcluir = linha.insertCell(5);
+
+  colunaIdChamado.innerHTML = idChamado;
+  colunaUnidade.innerHTML = unidade;
+  colunaDescricao.innerHTML = descricao;
+  colunaEstado.innerHTML = estado;
+  colunaEditar.innerHTML = '<i class="fas fa-edit edit-icon" onclick="editarChamado(this)"></i>';
+  colunaExcluir.innerHTML = '<i class="fas fa-trash delete-icon" onclick="confirmarExclusao(this)"></i>';
+
+  // Exibir o contêiner da tabela
+  document.getElementsByClassName("table-container")[0].style.display = "block";
+
+  // Limpar os campos de entrada após o registro
+  document.getElementById("id-chamado").value = "";
+  document.getElementById("unidade").value = "";
+  document.getElementById("descricao").value = "";
+  document.getElementById("estado").value = "";
+
+  // Salvar os dados atualizados
+  salvarDados();
+}
